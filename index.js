d7bb1ec2ae53bd15233fb036d2c5a2d49736ab25
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-05-14 22:53:59 +0600

    delte

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app=express();
 const port=process.env.PORT || 5000;
 
@@ -46,6 +46,22 @@ async function run() {
         const cursor=collectionBooks.find();
         const result=await cursor.toArray();
         res.send(result);
+    })
+
+    app.get("/books/:id", async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:new ObjectId(id)};
+        const result=await collectionBooks.findOne(query);
+        res.send(result)
+    })
+
+
+    app.delete("/books/:id",async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:new ObjectId(id)};
+        const result=await collectionBooks.deleteOne(query);
+        res.send(result);
+
     })
 
 
